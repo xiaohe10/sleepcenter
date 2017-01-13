c5510ca930728efec1e32bc1b4862e52dbfd9d24
xiaohe <xiaoh16@gmail.com>
2017-01-13 13:25:21 +0800

    initial upload

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/',function(req,res,next){
+  res.render('patientlist', { title: '病人列表' });
+});
+router.get('/medical_record', function(req, res, next) {
+  patientname = req.query.patientname;
+  res.render('medical_record', { title: '综合病历记录表',patientname:patientname});
+});
+router.get('/history', function(req, res, next) {
+  patientname = req.query.patientname;
+  res.render('history', { title: '历史记录',patientname:patientname});
+});
+router.get('/register', function(req, res, next) {
+  patientname = req.query.patientname;
+  res.render('register', { title: '信息登记',patientname:patientname});
+});
+router.get('/tables', function(req, res, next) {
+  table_name = req.query.table_name;
+  patientname:patientname;
+  res.render(table_name, { title: table_name,patientname:patientname});
+});
+module.exports = router;
